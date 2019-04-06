1b3ee8843a75a7b0c0bb0be54be7b791ec10544a
Lucas Marchesoti Franco <lucas_marchesoti@callink.com.br>
2019-04-06 17:27:41 -0300

    add support for CORS and send IDs for product and salesperson

---
+++ src/main/java/api/RepositoryConfig.java
@@ -0,0 +1,18 @@
+package api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import api.product.Product;
+import api.salesperson.Salesperson;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Product.class);
+        config.exposeIdsFor(Salesperson.class);
+    }
+}
